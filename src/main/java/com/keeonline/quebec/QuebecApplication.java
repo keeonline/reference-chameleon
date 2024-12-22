692713e454d046c1fa079946c8d52b2327c47aa8
keeonline <keeonline@outlook.com>
2024-12-22 10:35:13 +0000

    initial commit

---
+++ src/main/java/com/keeonline/quebec/QuebecApplication.java
@@ -0,0 +1,13 @@
+package com.keeonline.quebec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuebecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuebecApplication.class, args);
+	}
+
+}

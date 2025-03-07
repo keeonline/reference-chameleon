e201dbeb8e47abe1ef24eece1d0ff0448ebe103a
keeonline <keeonline@outlook.com>
2025-03-07 11:42:59 +0000

    wip

--- src/main/java/com/keeonline/quebec/api/controller/GreetingsController.java
+++ src/main/java/com/keeonline/quebec/api/controller/GreetingsController.java
@@ -1,5 +1,7 @@
 package com.keeonline.quebec.api.controller;
 
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.keeonline.quebec.api.model.GreetingsDto;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
+
 @RestController
 @RequestMapping("/public/quebec/greetings")
 public class GreetingsController {
 
     @GetMapping(path = "")
+    @WithSpan(value = "/greetings")
     public ResponseEntity<GreetingsDto> get() {
-            return ResponseEntity.ok().body(new GreetingsDto());
+
+        Span span = Span.current();
+        span.setAttribute("application.request_id",UUID.randomUUID().toString());
+
+        return ResponseEntity.ok().body(new GreetingsDto());
     }
     
 }

7aee8b3ab137b45191c13d1ca6edc2b4f28acc0b
keeonline <keeonline@outlook.com>
2025-11-11 15:49:19 +0000

    wip

--- deploy/aws/iam.tf
+++ deploy/aws/iam.tf
@@ -2,6 +2,6 @@ data "aws_iam_role" "ecs_task_exec" {
   name = "${var.infra_environment}-iam-role-ecs-task-exec"
 }
 
-data "aws_iam_role" "ecs_task" {
-  name = "${var.infra_environment}-iam-role-ecs-task"
-}
+# data "aws_iam_role" "ecs_task" {
+#   name = "${var.infra_environment}-iam-role-ecs-task"
+# }
--- deploy/aws/service-bravo.tf
+++ deploy/aws/service-bravo.tf
@@ -5,7 +5,7 @@ resource "aws_ecs_task_definition" "bravo" {
   cpu                      = 256
   memory                   = 512
   execution_role_arn       = data.aws_iam_role.ecs_task_exec.arn
-  task_role_arn            = data.aws_iam_role.ecs_task.arn
+  # task_role_arn            = data.aws_iam_role.ecs_task.arn
 
   container_definitions = jsonencode([
     {

071296450c8b483af46e5bc112fdb2b58728c5d0
keeonline <keeonline@outlook.com>
2025-11-12 06:34:11 +0000

    wip

--- deploy/aws/iam.tf
+++
@@ -1,7 +0,0 @@
-data "aws_iam_role" "ecs_task_exec" {
-  name = "${var.infra_environment}-iam-role-ecs-task-exec"
-}
-
-# data "aws_iam_role" "ecs_task" {
-#   name = "${var.infra_environment}-iam-role-ecs-task"
-# }

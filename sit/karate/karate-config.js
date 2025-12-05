cc31da0c1f8706457b98e2ba5923901857b6a320
keeonline <keeonline@outlook.com>
2025-12-05 08:35:56 +0000

    wip

--- sit/karate/karate-config.js
+++ sit/karate/karate-config.js
@@ -1,6 +1,7 @@
 function fn() {
     var config = {
-        baseUrl: karate.properties['baseurl'] || 'http://localhost'
+        baseUrl: karate.properties['baseurl'] || 'http://localhost',
+        appEnv: karate.properties['appenv'] || 'default'
     }
     karate.configure('retry', { count: 10, interval: 20000 });
     return config;
